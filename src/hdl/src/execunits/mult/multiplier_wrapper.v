afd3aea0645ee1155a4ae2acb19f453597be67cb
omonroy <omonroy@ae95d81c-294e-11df-9785-5baaa8915590>
2010-04-24 20:07:54 +0000

    multiplier wrapper

--- src/hdl/src/execunits/mult/multiplier_wrapper.v
+++ src/hdl/src/execunits/mult/multiplier_wrapper.v
@@ -2,7 +2,28 @@
 `ifndef MULTIPLIER_WRAPPER_V
 `define MULTIPLIER_WRAPPER_V
 
-module multiplier_wrapper();
+module multiplier_wrapper(
+         input clk,
+         input reset,
+         input  [31:0] issuemult_rsdata,
+         input  [31:0] issuemult_rtdata,
+         input  [ 5:0] issuemult_rdtag,
+         input         issuemult_enable,
+
+         output [31:0] issuemult_out,
+         output [ 5:0] issuemult_rdtag_out 
+   );
+
+assign issuemult_rdtag_out = issuemult_rdtag;
+
+multiplier multiplier (
+  .sclr  (reset),
+  .clk   (clk),
+  .a     (issuemult_rsdata),
+  .b     (issuemult_rtdata),
+  .p     (issuemult_out)
+  );
+
 endmodule
 
 `endif

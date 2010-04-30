1fc767ea30933b697bec9ac270f294844b5a70c8
omonroy <omonroy@ae95d81c-294e-11df-9785-5baaa8915590>
2010-04-30 02:48:30 +0000

    Issue unit

--- src/hdl/src/issue.v
+++ src/hdl/src/issue.v
@@ -32,9 +32,14 @@ module issue (
    output             cdb_branch,
    output             cdb_branch_taken
 );
-   reg  [7:0] cdb_slot;
-   wire [3:0] selector;
+
+   localparam INT_BEFORE_LOAD = 1;
+
+   reg  [7:0] cdb_slot, cdb_slot_r;
+   wire [3:0] mux_cdb_ctrl;
    reg        LRU;
+   reg  [5:0] div_cdb_ctrl, div_cdb_ctrl_r;
+   reg  [2:0] mult_cdb_ctrl, mult_cdb_ctrl_r;
 
    wire [31:0]   int_out;
    wire [31:0]   div_out;
@@ -44,74 +49,75 @@ module issue (
    wire div_exec_ready;
 
    //CDB reservation registers
-   always @(posedge clk) begin : cdb_slots
-      if (reset) begin
-         cdb_slot <= 7'h0;
-      end
-      else begin
-         cdb_slot [7] <= ready_div;
-         cdb_slot [6] <= cdb_slot[7];
-         cdb_slot [5] <= cdb_slot[6];
-         cdb_slot [4] <= cdb_slot[5] | ready_mult;
-         cdb_slot [3] <= cdb_slot[4];
-         cdb_slot [2] <= cdb_slot[3];
-         cdb_slot [1] <= cdb_slot[2] | (ready_int | ready_ld_buf);
-         cdb_slot [0] <= cdb_slot[1];
-      end
+   always @(*) begin : cdb_slots_proc
+         cdb_slot [6] = ready_div;
+         cdb_slot [5] = cdb_slot_r[6];
+         cdb_slot [4] = cdb_slot_r[5];
+         cdb_slot [3] = cdb_slot_r[4] | ready_mult;
+         cdb_slot [2] = cdb_slot_r[3];
+         cdb_slot [1] = cdb_slot_r[2];
+         cdb_slot [0] = cdb_slot_r[1] | ready_int | ready_ld_buf;
    end
 
-   // ISSUE UNIT LOGIC
-   //1. Check if ready signal is activated by the exec queues
-   //   can be written into reservation table. 
-   //   Check if prior locality entry point on the exec unit
-   //   isn't 1.
-   //2. In case of divisions: if div_exec_ready(busy) is active high
-   //   can't write on the table and issue_div_done cannot be asserted
-   //3. If the ready signals are present, the integer exec queue and
-   //   memory access, we need to arbitrate using a bit of LRU
+   always @(posedge clk) begin : cdb_slots_reg_assign
+         cdb_slot_r [6] <= (reset) ? 0 :  cdb_slot [6] ;
+         cdb_slot_r [5] <= (reset) ? 0 :  cdb_slot [5] ;
+         cdb_slot_r [4] <= (reset) ? 0 :  cdb_slot [4] ;
+         cdb_slot_r [3] <= (reset) ? 0 :  cdb_slot [3] ;
+         cdb_slot_r [2] <= (reset) ? 0 :  cdb_slot [2] ;
+         cdb_slot_r [1] <= (reset) ? 0 :  cdb_slot [1];
+         cdb_slot_r [0] <= (reset) ? 0 :  cdb_slot [0];
+   end
+
    always @(*) begin: issue_unit_logic
-      if (reset) begin
-         issue_int    = 1'b0;
-         issue_mult   = 1'b0;
-         issue_div    = 1'b0;
-         issue_ld_buf = 1'b0;
-      end
-      else begin
-         if (ready_int && cdb_slot[2] != 1'b1 ) begin
-            issue_int = 1'b1;
-         end
-         else if (ready_mult && cdb_slot[5] != 1'b1) begin
-            issue_mult = 1'b1;
-         end
-         else if (ready_div && div_exec_ready != 1'b1) begin
-            issue_div = 1'b1;
-         end
-         else if (ready_ld_buf && cdb_slot[2] != 1'b1) begin
-            issue_ld_buf = 1'b1;
-         end
-         else begin
-            if (ready_int && ready_ld_buf) begin
-                //LRU = 1'b0;
-            end
-            else begin
-               //LRU = 1'b1;
-            end
-         end
-      end
+      issue_int   =  (~cdb_slot_r[1] & ready_int)    & (~ready_ld_buf |  INT_BEFORE_LOAD);
+      issue_ld_buf = (~cdb_slot_r[1] & ready_ld_buf) & (~ready_int    | ~INT_BEFORE_LOAD);
+      issue_div    = div_exec_ready & ready_div;
+      issue_mult   = ~cdb_slot_r[4] & ready_mult;
    end
 
-   assign selector = {ready_int, ready_mult, ready_div, ready_ld_buf};
+   always @(posedge clk) begin: div_reg_assign
+         div_cdb_ctrl_r[5] <= (reset) ? 0 : div_cdb_ctrl[5];
+         div_cdb_ctrl_r[4] <= (reset) ? 0 : div_cdb_ctrl[4];
+         div_cdb_ctrl_r[3] <= (reset) ? 0 : div_cdb_ctrl[3];
+         div_cdb_ctrl_r[2] <= (reset) ? 0 : div_cdb_ctrl[2];
+         div_cdb_ctrl_r[1] <= (reset) ? 0 : div_cdb_ctrl[1];
+         div_cdb_ctrl_r[0] <= (reset) ? 0 : div_cdb_ctrl[0];
+   end
 
-   always @(*) begin : mux_sel
-      case (selector)
+   always @(*) begin : div_delay_proc
+         div_cdb_ctrl [5] = issue_div;
+         div_cdb_ctrl [4] = div_cdb_ctrl [5];
+         div_cdb_ctrl [3] = div_cdb_ctrl [4];
+         div_cdb_ctrl [2] = div_cdb_ctrl [3];
+         div_cdb_ctrl [1] = div_cdb_ctrl [2];
+         div_cdb_ctrl [0] = div_cdb_ctrl [1];
+   end
+
+   always @(posedge clk) begin: mult_reg_assign
+         mult_cdb_ctrl_r[2] <= (reset) ? 0 : mult_cdb_ctrl[2];
+         mult_cdb_ctrl_r[1] <= (reset) ? 0 : mult_cdb_ctrl[1];
+         mult_cdb_ctrl_r[0] <= (reset) ? 0 : mult_cdb_ctrl[0];
+   end
+
+   always @(*) begin : mult_delay_proc
+         mult_cdb_ctrl [2] = issue_mult;
+         mult_cdb_ctrl [1] = mult_cdb_ctrl [2];
+         mult_cdb_ctrl [0] = mult_cdb_ctrl [1];
+   end
+
+   assign mux_cdb_ctrl = {issue_int, div_cdb_ctrl_r[0], mult_cdb_ctrl_r[0], issue_ld_buf};
+
+   always @(*) begin : mux_cdb_out
+      case (mux_cdb_ctrl)
          4'b0001: begin
            cdb_out = ld_buf_out; 
          end
          4'b0010: begin
-           cdb_out = div_out; 
+           cdb_out = mult_out; 
          end
          4'b0100: begin
-            cdb_out = mult_out;
+            cdb_out = div_out;
          end
          4'b1000: begin
             cdb_out = int_out;
@@ -155,7 +161,7 @@ module issue (
          .issuediv_rdtag_out  (cdb_tagout)
    );
    //multiplier exec unit
-   multiplier_wrapper multiplier_wrapper(
+/*   multiplier_wrapper multiplier_wrapper(
       .clk                 (clk        ),
       .reset               (reset      ),
       .issuemult_rsdata    (rsdata     ),
@@ -164,7 +170,7 @@ module issue (
 
       .issuemult_out       (mult_out   ),
       .issuemult_rdtag_out (cdb_tagout )
-   ); /*
+   );
    //load/store exec unit
    issuels issuels(
       .clk              (clk           ),

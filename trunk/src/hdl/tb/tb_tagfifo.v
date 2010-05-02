129fd1e26dde5225e71116dff58d6eab65e5ebd3
betodelrio <betodelrio@ae95d81c-294e-11df-9785-5baaa8915590>
2010-05-02 02:18:50 +0000

    Adding clocking block.

--- trunk/src/hdl/tb/tb_tagfifo.v
+++ trunk/src/hdl/tb/tb_tagfifo.v
@@ -24,11 +24,11 @@ module tb_tagfifo();
       //-----------------------------------------------------------------------
       // Case 0: don't take anything, let tagfifo full.
       //-----------------------------------------------------------------------
-      @(posedge cb.clk);
-      dispatch_ren = 0;
-      cdb_tag      = 0;
-      cdb_valid    = 0;
-      repeat (10) @(posedge cb.clk);
+      @(posedge clk);
+      cb.dispatch_ren <= 0;
+      cb.cdb_tag      <= 0;
+      cb.cdb_valid    <= 0;
+      repeat (10) @(posedge clk);
       reset_testbench(5);
 
 
@@ -36,45 +36,45 @@ module tb_tagfifo();
       // Case 1: take all tags from queue, then insert them but in reverse
       //         order.
       //-----------------------------------------------------------------------
-      @(posedge cb.clk);
-      wait (dispatch_full);
-      dispatch_ren = 1;
-      cdb_tag      = 0;
-      cdb_valid    = 0;
+      @(posedge clk);
+      wait (cb.dispatch_full);
+      cb.dispatch_ren <= 1;
+      cb.cdb_tag      <= 0;
+      cb.cdb_valid    <= 0;
       while (~dispatch_empty) begin
-         @(posedge cb.clk);
+         @(posedge clk);
       end
-      dispatch_ren = 0;
+      cb.dispatch_ren <= 0;
 
       for (i = 63; i >= 0; i = i - 1) begin
-         cdb_tag   = i;
-         cdb_valid = 1;
-         @(posedge cb.clk);
+         cb.cdb_tag   <= i;
+         cb.cdb_valid <= 1;
+         @(posedge clk);
       end
-      cdb_valid = 0;
+      cb.cdb_valid <= 0;
 
-      repeat (10) @(posedge cb.clk);
+      repeat (10) @(posedge clk);
       reset_testbench(5);
 
 
       //-----------------------------------------------------------------------
       // Case 2: pushing and poping at the same time.
       //-----------------------------------------------------------------------
-      @(posedge cb.clk);
-      wait (dispatch_full);
-      dispatch_ren = 1;
-      cdb_tag      = 0;
-      cdb_valid    = 0;
-      repeat (5) @(posedge cb.clk);
-      dispatch_ren = 0;
+      @(posedge clk);
+      wait (cb.dispatch_full);
+      cb.dispatch_ren <= 1;
+      cb.cdb_tag      <= 0;
+      cb.cdb_valid    <= 0;
+      repeat (5) @(posedge clk);
+      cb.dispatch_ren <= 0;
       for (int i = 0; i < 5; i = i + 1) begin
-         cdb_tag   = i * 7;
-         cdb_valid = 1;
-         @(posedge cb.clk);
+         cb.cdb_tag   <= i * 7;
+         cb.cdb_valid <= 1;
+         @(posedge clk);
       end
-      cdb_valid = 0;
+      cb.cdb_valid <= 0;
 
-      repeat (10) @(posedge cb.clk);
+      repeat (10) @(posedge clk);
       reset_testbench(5);
 
 
@@ -83,27 +83,28 @@ module tb_tagfifo();
 
 
    task reset_testbench(integer unsigned cycles);
-      dispatch_ren = 0;
-      cdb_tag      = 0;
-      cdb_valid    = 0;
-      reset = 1'b1;
-      repeat (cycles) @(posedge cb.clk);
-      reset = 1'b0;
+      cb.dispatch_ren <= 0;
+      cb.cdb_tag      <= 0;
+      cb.cdb_valid    <= 0;
+      cb.reset <= 1'b1;
+      repeat (cycles) @(posedge clk);
+      cb.reset <= 1'b0;
    endtask
 
    initial begin
-      cb.clk = 1'b0;
-      forever #5ns cb.clk <= ~cb.clk;
+      clk = 1'b0;
+      forever #5ns clk <= ~clk;
    end
 
    clocking cb @(posedge clk);
       default input #1ns output #2ns;
-      input  cdb_tag;
-      input  cdb_valid;
-      input  dispatch_ren;
-      output dispatch_tag;
-      output dispatch_full;
-      output dispatch_empty;
+      output  reset;
+      output cdb_tag;
+      output cdb_valid;
+      output dispatch_ren;
+      input  dispatch_tag;
+      input  dispatch_full;
+      input  dispatch_empty;
    endclocking
 
    tagfifo tagfifo (

f4bc6261df14ed1c65f7c5114fec0c1449d8a363
omonroy <omonroy@ae95d81c-294e-11df-9785-5baaa8915590>
2010-05-02 00:50:46 +0000

    Issue unit complete. Data cache, div, int

--- trunk/src/hdl/src/dcache.v
+++ trunk/src/hdl/src/dcache.v
@@ -11,23 +11,16 @@ module dcache (
    input  wire [15:0] addr,
    input  wire [31:0] wdata,
    input  wire [ 5:0] tag_in,
-   input              ls_ready_in,
-   input              ls_done_in,
-   output reg         ls_done_out,
-   output reg         ls_ready_out,
    output reg  [ 5:0] tag_out,
    output wire [31:0] rdata
 );
 
    always @(*) begin: ls_ready_proc
-      ls_ready_out = ls_ready_in;
-      ls_done_out  = ls_done_in;
       tag_out      = tag_in;
    end
 
    dcache_mem dcache_mem (
       .clka  ( clk   ),
-      .ena   ( en    ),
       .wea   ( wen   ),
       .addra ( addr  ),
       .dina  ( wdata ),

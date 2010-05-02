061ff82d3a49e2f80d30bb4a6fcacaf06df124ed
betodelrio <betodelrio@ae95d81c-294e-11df-9785-5baaa8915590>
2010-05-02 00:00:17 +0000

    Instruction cache and instruction fetch queue are now working properly, even when branching.

--- src/hdl/src/icache.v
+++ src/hdl/src/icache.v
@@ -9,39 +9,42 @@
 module icache #(
    parameter integer W_DATA      = 128,
    parameter integer W_ADDR      =  32,
-   parameter integer N_BYTEALIGN =   4
+   parameter integer W_BYTEALIGN =   4
 ) (
-   input              clk,
-   input              reset,
-   input      [31:0]  ifq_pcin,
-   input              ifq_ren,
-   input              ifq_abort,
-   output reg [127:0] ifq_dout,
-   output reg         ifq_dout_valid
+   input                   clk,
+   input                   reset,
+   input      [W_ADDR-1:0] ifq_pcin,
+   input                   ifq_ren,
+   input                   ifq_abort,
+   output reg [W_DATA-1:0] ifq_dout,
+   output reg              ifq_dout_valid
 );
 
+   localparam integer W_ENTRY = 6;
+
    // Cache line selector.
-   reg [W_ADDR-N_BYTEALIGN-1:0] line_sel;
+   reg [W_ENTRY-1:0] line_sel;
 
    reg dout_valid, dout_valid_r;
 
-   always @(*) begin : icache_read_proc
+   always @(*) begin : read_proc
       // Addresses are aligned to N_BYTEALIGN bytes, ignore the LSB.
-      line_sel = ifq_pcin[W_ADDR-1:N_BYTEALIGN];
+      line_sel = ifq_pcin[W_ENTRY + W_BYTEALIGN - 1 : W_BYTEALIGN];
 
-      dout_valid = ifq_ren & ~ifq_abort;
+      dout_valid = ifq_ren;
    end
 
-   always @(*) begin : icache_oreg_assign
+   always @(*) begin : oreg_assign
       ifq_dout_valid = dout_valid_r & ~ifq_abort;
    end
 
-   always @(posedge clk) begin : icache_dout_valid_proc
+   always @(posedge clk) begin : dout_valid_reg
       dout_valid_r <= (reset) ? 0 : dout_valid;
    end
 
    //
-   // NOTE: Read only memory (W_DATA) x (2 ^ W_ADDR).
+   // NOTE: Read only memory (W_DATA) x (2 ^ W_ADDR). Due to FPGA limitations
+   //       it only addresses W_ENTRY entries.
    //
    icache_mem icache_mem (
       .clka  ( clk      ),
--- src/hdl/src/ifq.v
+++ src/hdl/src/ifq.v
@@ -22,9 +22,6 @@ module ifq (
    input              dispatch_branch_valid
 );
 
-   //
-   // TODO: replace with a RAM block for FPGA implementation.
-   //
    // Internal FIFO memory, 128x4.
    reg  [127:0] mem   [3:0];
    reg  [127:0] mem_r [3:0];
@@ -32,35 +29,48 @@ module ifq (
    // Read and write pointers.
    reg  [  4:0] wptr, wptr_r;
    reg  [  4:0] rptr, rptr_r;
+   reg  [  4:0] rptr_branch, wptr_branch;
 
    reg  [ 31:0] pcin,  pcin_r;
    reg  [ 31:0] pcout, pcout_r;
+   reg  [ 31:0] pcout_branch, pcin_branch;
 
-   reg is_full, is_empty;
-   reg is_valid_read,  do_inc_rptr;
-   reg is_valid_write, do_inc_wptr;
-   reg bypass_mux_sel;
-   always @(*) begin : fifo_proc
+   reg icache_req, icache_req_r;
+   reg dispatch_req;
+   reg is_full, will_be_full, is_empty, will_be_empty;
+   reg do_inc_rptr, do_inc_wptr;
+   reg do_bypass_mem;
+
+   always @(*) begin : fifo_full_empty_proc
       is_empty = (wptr_r[4] == rptr_r[4]) && (wptr_r[3:2] == rptr_r[3:2]);
       is_full  = (wptr_r[4] != rptr_r[4]) && (wptr_r[3:2] == rptr_r[3:2]);
+      will_be_empty = (wptr[4] == rptr[4]) && (wptr[3:2] == rptr[3:2]);
+      will_be_full  = (wptr[4] != rptr[4]) && (wptr[3:2] == rptr[3:2]);
+   end
 
-      bypass_mux_sel = dispatch_branch_valid | is_empty;
-      is_valid_read  = dispatch_ren      & ~is_empty;
-      is_valid_write = icache_dout_valid & ~is_full;
-      do_inc_rptr    = is_valid_read | bypass_mux_sel;
-      do_inc_wptr    = is_valid_write;
+   always @(*) begin : fifo_ptr_proc
+      do_bypass_mem = is_empty & icache_req_r;
+      dispatch_req = (~is_empty & dispatch_ren) | do_bypass_mem;
+      icache_req = ~will_be_full;
+
+      do_inc_rptr = dispatch_req;
+      do_inc_wptr = icache_req_r & icache_dout_valid;
 
       // If a branch/jump is detected the IFQ must be emptied, that is,
       // both write/read pointers must point to the same address (0).
       // Read from icache 4 instructions at a time, and write to dispatch unit
       // only one instruction at a time.
-      rptr = (dispatch_branch_valid) ? 'h0 : (do_inc_rptr) ? rptr_r + 1 : rptr_r;
-      wptr = (dispatch_branch_valid) ? 'h0 : (do_inc_wptr) ? wptr_r + 4 : wptr_r;
+      rptr_branch = {3'h0, dispatch_branch_addr[1:0]};
+      wptr_branch = {5'h0};
+      rptr = (dispatch_branch_valid) ? rptr_branch : ( (do_inc_rptr) ? rptr_r + 1 : rptr_r );
+      wptr = (dispatch_branch_valid) ? wptr_branch : ( (do_inc_wptr) ? wptr_r + 4 : wptr_r );
 
       // 4 byte alignment for PC.
       // When no branch/jump, read 1 cache line (4 instructions at a time) from icache.
-      pcout = (dispatch_branch_valid) ? dispatch_branch_addr +  4 : (do_inc_rptr) ? pcout_r + 4 : pcout_r;
-      pcin  = (dispatch_branch_valid) ? dispatch_branch_addr + 16 : (do_inc_wptr) ? pcin_r + 16 : pcin_r;
+      pcout_branch = {dispatch_branch_addr[31:2], 2'h0};
+      pcin_branch  = {dispatch_branch_addr[31:4], 4'h0};
+      pcout = (dispatch_branch_valid) ? pcout_branch : ( (do_inc_rptr) ? pcout_r + 4 : pcout_r );
+      pcin  = (dispatch_branch_valid) ? pcin_branch  : ( (icache_req)  ? pcin_r + 16 : pcin_r );
    end
 
    // Internal mux signals, one coming form memory and the other coming
@@ -84,7 +94,7 @@ module ifq (
          2'b11: inst_from_mem_mux_out = line_from_mem[127:96];
       endcase
 
-      case (bypass_mux_sel)
+      case (do_bypass_mem)
          1'b0: bypass_mux_out = inst_from_mem_mux_out;
          1'b1: bypass_mux_out = inst_from_input_mux_out;
       endcase
@@ -99,9 +109,9 @@ module ifq (
       //       icache_dout_valid hasn't arrived yet.
       //       For now, we never set icache_abort.
       //
-      icache_abort = 1'b0;
+      icache_abort = dispatch_branch_valid & icache_req_r;
       icache_pcin  = (dispatch_branch_valid) ? dispatch_branch_addr : pcin_r;
-      icache_ren   = ~(dispatch_branch_valid | is_full);
+      icache_ren   = icache_req;
 
       dispatch_pcout_plus4 = (dispatch_branch_valid) ? pcout : pcout_r;
       dispatch_inst        = bypass_mux_out;
@@ -112,17 +122,21 @@ module ifq (
       integer i;
       for(i = 0; i < 4; i = i + 1) mem[i] = mem_r[i];
 
-      mem[wptr_r[3:2]] = (icache_dout_valid) ? icache_dout : mem_r[wptr_r[3:2]];
+      mem[wptr_r[3:2]] = (icache_dout_valid & icache_req_r) ? icache_dout : mem_r[wptr_r[3:2]];
+   end
+
+   always @(posedge clk) begin : ifq_flags_reg
+      icache_req_r   <= (reset) ? 'h0 : icache_req;
    end
 
    always @(posedge clk) begin : ifq_ptr_reg
-      rptr_r <= (reset) ? 5'b0 : rptr;
-      wptr_r <= (reset) ? 5'b0 : wptr;
+      rptr_r <= (reset) ? 'h0 : rptr;
+      wptr_r <= (reset) ? 'h0 : wptr;
    end
 
    always @(posedge clk) begin : ifq_pc_reg
-      pcin_r  <= (reset) ? 32'b0 : pcin;
-      pcout_r <= (reset) ? 32'b0 : pcout;
+      pcin_r  <= (reset) ? 'h0 : pcin;
+      pcout_r <= (reset) ? 'h0 : pcout;
    end
 
    always @(posedge clk) begin : ifq_mem_reg

a5e8f798181beea55ad0c4785d39dac4b3506dc2
betodelrio <betodelrio@ae95d81c-294e-11df-9785-5baaa8915590>
2010-05-06 02:15:41 +0000

    Issue unit now takes 3 cycles for multiplications and 6 for divisions.

--- trunk/src/hdl/src/issue.v
+++ trunk/src/hdl/src/issue.v
@@ -4,6 +4,8 @@
 
 `timescale 1ns/1ps
 
+`include "globals.vh"
+
 module issue (
       input                         clk,
       input                         reset,
@@ -46,183 +48,193 @@ module issue (
       output reg                    cdb_branch_taken
 
 );
-   reg   issuemult_equeuemult_done_r;
-   reg   issue_int;
-   reg   issue_mult;
-   reg   issue_div;
-   reg   issue_ld_buf,  issue_ld_buf_r;
+   localparam integer SEL_LS   = 4'b0001;
+   localparam integer SEL_MULT = 4'b0010;
+   localparam integer SEL_DIV  = 4'b0100;
+   localparam integer SEL_INT  = 4'b1000;
 
-   reg        issuediv_equeuediv_done_r;
-   reg  [6:0] cdb_slot, cdb_slot_r;
-   wire [3:0] mux_cdb_ctrl;
-   reg        int_before_ls, int_before_ls_r;
-   reg  [5:0] div_cdb_ctrl, div_cdb_ctrl_r;
-   reg  [2:0] mult_cdb_ctrl, mult_cdb_ctrl_r;
+   // Output register for CDB.
+   reg  [31:0] cdb_data_oreg,         cdb_data_oreg_r;
+   reg  [ 5:0] cdb_tag_oreg,          cdb_tag_oreg_r;
+   reg         cdb_valid_oreg,        cdb_valid_oreg_r;
+   reg         cdb_branch_oreg,       cdb_branch_oreg_r;
+   reg         cdb_branch_taken_oreg, cdb_branch_taken_oreg_r;
 
-   wire [31:0]   issueint_out;
-   wire [31:0]   issuediv_out;
-   wire [31:0]   issuemult_out;
-   wire [31:0]   issuels_out;
+   reg  [ 3:0] cdb_mux_sel;
+   reg  [ 6:0] cdb_slot, cdb_slot_r;
+   reg         int_before_ls, int_before_ls_r;
 
-   wire div_exec_ready, issueint_alubranch, issueint_alubranch_taken ;
-   wire [5:0] issuels_tagout, issueint_tagout, issuediv_tagout, issuemult_tagout;
-   //CDB reservation registers
-   always @(*) begin : cdb_slots_proc
-         cdb_slot [6] = issuediv_ready;
-         cdb_slot [5] = cdb_slot_r[6];
-         cdb_slot [4] = cdb_slot_r[5];
-         cdb_slot [3] = cdb_slot_r[4] | issuemult_ready;
-         cdb_slot [2] = cdb_slot_r[3];
-         cdb_slot [1] = cdb_slot_r[2];
-         cdb_slot [0] = cdb_slot_r[1] | issueint_ready | issuels_ready;
+   // Division takes 6 clock cycles, not pipelined, and multiplication takes
+   // only 3 cycles, pipelined.
+   reg  [ 5:0] div_cdb_ctrl,  div_cdb_ctrl_r;
+   reg  [ 2:0] mult_cdb_ctrl, mult_cdb_ctrl_r;
+
+   wire [31:0] issuemult_out,  issuediv_out,  issueint_out,  issuels_out;
+   reg         can_issue_mult, can_issue_div, can_issue_int, can_issue_ls;
+
+   wire        div_exec_ready, issueint_alubranch, issueint_alubranch_taken ;
+   wire [ 5:0] issuels_tagout, issueint_tagout, issuediv_tagout, issuemult_tagout;
+
+   // Temp register used for behavioral multiplication instead of its execution
+   // unit. Since multiplication takes 3 cycles and it is pipelined, we need to
+   // store all results.
+   reg  [31:0] issuemult_out_temp   [2:0];
+   reg  [31:0] issuemult_out_temp_r [2:0];
+   reg  [31:0] issuemult_tag_temp   [2:0];
+   reg  [31:0] issuemult_tag_temp_r [2:0];
+
+   // If a load/store contend with an integer operation, give priority to the
+   // integer operation. The next time both types contend, then give priority
+   // to load/store operation. That is, priority toggles each time both time of
+   // operations contend.
+   always @(*) begin: issue_int_before_load_proc
+      int_before_ls = int_before_ls_r ^ (issueint_ready & issuels_ready);
    end
 
-   always @(posedge clk) begin : cdb_slots_reg_assign
-         cdb_slot_r [6] <= (reset) ? 0 :  cdb_slot [6] ;
-         cdb_slot_r [5] <= (reset) ? 0 :  cdb_slot [5] ;
-         cdb_slot_r [4] <= (reset) ? 0 :  cdb_slot [4] ;
-         cdb_slot_r [3] <= (reset) ? 0 :  cdb_slot [3] ;
-         cdb_slot_r [2] <= (reset) ? 0 :  cdb_slot [2] ;
-         cdb_slot_r [1] <= (reset) ? 0 :  cdb_slot [1];
-         cdb_slot_r [0] <= (reset) ? 0 :  cdb_slot [0];
+   // CDB reservation registers
+   always @(*) begin : issue_cdb_reservation_proc
+      can_issue_div  = issuediv_ready;
+      can_issue_mult =  ~cdb_slot_r[4] & issuemult_ready & div_exec_ready;
+      can_issue_int  = (~cdb_slot_r[1] & issueint_ready) & (~issuels_ready  | ~int_before_ls_r);
+      can_issue_ls   = (~cdb_slot_r[1] & issuels_ready ) & (~issueint_ready |  int_before_ls_r);
+
+      cdb_slot[6] = can_issue_div;
+      cdb_slot[5] = cdb_slot_r[6];
+      cdb_slot[4] = cdb_slot_r[5];
+      cdb_slot[3] = cdb_slot_r[4] | can_issue_mult;
+      cdb_slot[2] = cdb_slot_r[3];
+      cdb_slot[1] = cdb_slot_r[2];
+      cdb_slot[0] = cdb_slot_r[1] | can_issue_int | can_issue_ls;
    end
 
    //
-   // TODO: change the INT_BEFORE_LOAD parameter into a register. If a
-   //       load/store contend with an integer operation, give priority
-   //       to the integer operation. The next time both types contend,
-   //       then give priority to load/store operation.
-   //       That is, priority toggles each time both time of operations
-   //       contend.
+   // TODO: Replace behavioral multiplication unit by its RTL module.
    //
-   always @(posedge clk) begin : int_before_ls_reg_assign
-      int_before_ls_r <= (reset) ? 1'b0 : int_before_ls;
+   always @(*) begin : issue_mult_out_proc
+      issuemult_out_temp[2] = (can_issue_mult) ? issuemult_rsdata * issuemult_rtdata : 'h0;
+      issuemult_out_temp[1] = issuemult_out_temp_r[2];
+      issuemult_out_temp[0] = issuemult_out_temp_r[1];
+
+      issuemult_tag_temp[2] = (can_issue_mult) ? issuemult_rdtag : 'h0;
+      issuemult_tag_temp[1] = issuemult_tag_temp_r[2];
+      issuemult_tag_temp[0] = issuemult_tag_temp_r[1];
    end
 
-   always @(*) begin: issue_unit_logic
-      int_before_ls  = int_before_ls_r ^ (issueint_ready & issuels_ready);
-     // issue_int      = (~cdb_slot_r[1] & issueint_ready) & (~issuels_ready  | int_before_ls_r);
-     // issue_ld_buf   = (~cdb_slot_r[1] & issuels_ready ) & (~issueint_ready | ~int_before_ls_r);
-      issue_int      = (~cdb_slot_r[1] & issueint_ready) & (~issuels_ready  | ~int_before_ls_r);
-      issue_ld_buf   = (~cdb_slot_r[1] & issuels_ready ) & (~issueint_ready |  int_before_ls_r);
-      issue_div      = (cdb_slot_r[6]==1);
-      issue_mult     = ~cdb_slot_r[4] & issuemult_ready ;
+   // Delay lines for execution units that take longer than 1 clock cycle.
+   // Multiplication takes 3 cycles and division takes 6.
+   always @(*) begin : issue_execunit_sel_proc
+      div_cdb_ctrl[5] = can_issue_div;
+      div_cdb_ctrl[4] = div_cdb_ctrl_r[5];
+      div_cdb_ctrl[3] = div_cdb_ctrl_r[4];
+      div_cdb_ctrl[2] = div_cdb_ctrl_r[3];
+      div_cdb_ctrl[1] = div_cdb_ctrl_r[2];
+      div_cdb_ctrl[0] = div_cdb_ctrl_r[1];
+
+      mult_cdb_ctrl[2] = can_issue_mult;
+      mult_cdb_ctrl[1] = mult_cdb_ctrl_r[2];
+      mult_cdb_ctrl[0] = mult_cdb_ctrl_r[1];
    end
 
-   always @(posedge clk) begin: div_reg_assign
-         div_cdb_ctrl_r[5] <= (reset) ? 0 : div_cdb_ctrl[5];
-         div_cdb_ctrl_r[4] <= (reset) ? 0 : div_cdb_ctrl[4];
-         div_cdb_ctrl_r[3] <= (reset) ? 0 : div_cdb_ctrl[3];
-         div_cdb_ctrl_r[2] <= (reset) ? 0 : div_cdb_ctrl[2];
-         div_cdb_ctrl_r[1] <= (reset) ? 0 : div_cdb_ctrl[1];
-         div_cdb_ctrl_r[0] <= (reset) ? 0 : div_cdb_ctrl[0];
+   always @(*) begin : issue_oreg_proc
+      issuemult_equeuemult_done = can_issue_mult;
+      issuediv_equeuediv_done   = can_issue_div;
+      issueint_equeueint_done   = can_issue_int;
+      issuels_equeuels_done     = can_issue_ls;
+
+      cdb_valid        = cdb_valid_oreg_r;
+      cdb_data         = cdb_data_oreg_r;
+      cdb_tag          = cdb_tag_oreg_r;
+      cdb_branch       = cdb_branch_oreg_r;
+      cdb_branch_taken = cdb_branch_taken_oreg_r;
    end
 
-   always @(*) begin : div_delay_proc
-         div_cdb_ctrl [5] = issue_div;
-         div_cdb_ctrl [4] = div_cdb_ctrl_r [5];
-         div_cdb_ctrl [3] = div_cdb_ctrl_r [4];
-         div_cdb_ctrl [2] = div_cdb_ctrl_r [3];
-         div_cdb_ctrl [1] = div_cdb_ctrl_r [2];
-         div_cdb_ctrl [0] = div_cdb_ctrl_r [1];
-   end
-
-   always @(posedge clk) begin: mult_reg_assign
-         mult_cdb_ctrl_r[2] <= (reset) ? 0 : mult_cdb_ctrl[2];
-         mult_cdb_ctrl_r[1] <= (reset) ? 0 : mult_cdb_ctrl[1];
-         mult_cdb_ctrl_r[0] <= (reset) ? 0 : mult_cdb_ctrl[0];
-   end
-
-   always @(*) begin : mult_delay_proc
-         mult_cdb_ctrl [2] = issue_mult;
-         mult_cdb_ctrl [1] = mult_cdb_ctrl_r [2];
-         mult_cdb_ctrl [0] = mult_cdb_ctrl_r [1];
-   end
-   always @(posedge clk) begin: div_done_reg_assign
-      issuediv_equeuediv_done_r <= (reset) ? 0 :issuediv_equeuediv_done;
-   end
-   always @(*) begin : div_done
-      issuediv_equeuediv_done = (div_cdb_ctrl[0]) ? 1'b1: 1'b0;
-   end
-
-   always @(posedge clk) begin: issue_ld_buf_reg_assign
-      issue_ld_buf_r <= (reset) ? 0: issue_ld_buf;
-   end
-   always @(*)  begin: issue_ld_buf_process
-      issue_ld_buf = issue_ld_buf_r;
-   end
-
-   assign mux_cdb_ctrl = {issue_int, div_cdb_ctrl_r[0], issuemult_equeuemult_done_r, issue_ld_buf_r};
-
-   reg [31:0] mult_temp, mult_temp_r;
-
-
-   always @(posedge clk) begin : mult_temp_reg_assign
-      mult_temp_r <= (reset) ? 0: issuemult_rsdata * issuemult_rtdata;
-   end
-   always @(*) begin: mult_temp_proc
-      mult_temp = mult_temp_r;
-   end
-
-   always @(*) begin : mux_cdb_out
-      cdb_valid                = 1'b0;
-      cdb_data                 = 0;
-      cdb_tag                  = 0;
-      cdb_branch               = 1'b0;
-      cdb_branch_taken         = 1'b0;
-
-      case (mux_cdb_ctrl)
-         // only ls out
-         4'b0001: begin
-           cdb_data   = (issuels_opcode) ? 'h0: issuels_out;
-           cdb_tag    = (issuels_opcode) ? 'h0: issuels_tagout;
-           cdb_valid  = 1'b1;;
+   always @(*) begin : issue_cdb_mux_sel_proc
+      cdb_mux_sel = {can_issue_int, div_cdb_ctrl_r[0], mult_cdb_ctrl_r[0], can_issue_ls};
+      case (cdb_mux_sel)
+         SEL_LS : begin
+           cdb_valid_oreg  = 1'b1;
+           cdb_data_oreg   = (`ISSUELS_FUNC_SW == issuels_opcode) ? 'h0 : issuels_out;
+           cdb_tag_oreg    = (`ISSUELS_FUNC_SW == issuels_opcode) ? 'h0 : issuels_tagout;
          end
-         //only mult out
-         4'b0010: begin
-           //cdb_data    = issuemult_out; 
-           cdb_data    = (issuemult_equeuemult_done_r) ? mult_temp_r: cdb_data; 
-           cdb_tag = issuemult_tagout;
-           cdb_valid  = 1'b1;
+         SEL_MULT : begin
+           cdb_valid_oreg = 1'b1;
+           cdb_tag_oreg   = issuemult_tag_temp_r[0];
+           cdb_data_oreg  = issuemult_out_temp_r[0];
          end
          //only div out
 
-         4'b0100: begin
-            cdb_data    = issuediv_out;
-            cdb_tag     = issuediv_tagout;
-            cdb_valid   = (issuediv_equeuediv_done_r) ? 1'b1: 1'b0;
+         SEL_DIV : begin
+            cdb_valid_oreg = 1'b1;
+            cdb_tag_oreg   = issuediv_tagout;
+            cdb_data_oreg  = issuediv_out;
          end
          //only int out
-         4'b1000: begin
-            cdb_data    = issueint_out;
-            cdb_tag = issueint_tagout;
-            cdb_valid  = (issueint_alubranch)? 1'b0: 1'b1;
-            cdb_branch = issueint_alubranch;
-            cdb_branch_taken = issueint_alubranch_taken;
+         SEL_INT : begin
+            cdb_valid_oreg        = 1'b1;
+            cdb_data_oreg         = issueint_out;
+            cdb_tag_oreg          = issueint_tagout;
+            cdb_branch_oreg       = issueint_alubranch;
+            cdb_branch_taken_oreg = issueint_alubranch_taken;
          end
-         default: begin
-            cdb_data  = cdb_data;
-            cdb_tag   = cdb_tag;
-            cdb_valid = cdb_valid;
+         default : begin
+            cdb_valid_oreg        = 'h0;
+            cdb_data_oreg         = 'h0;
+            cdb_tag_oreg          = 'h0;
+            cdb_branch_oreg       = 'h0;
+            cdb_branch_taken_oreg = 'h0;
          end
       endcase
    end
-   always @(*) begin: int_mult_done
-      issueint_equeueint_done   = issue_int;
-      issuemult_equeuemult_done = (cdb_slot_r[1]==1 && issuemult_ready);
-      issuels_equeuels_done     = (cdb_slot_r[0]==1);
+
+   // Pipelined multiplication registers.
+   always @(posedge clk) begin : issuemult_out_temp_reg
+      issuemult_out_temp_r[2] <= (reset) ? 'h0 : issuemult_out_temp[2];
+      issuemult_out_temp_r[1] <= (reset) ? 'h0 : issuemult_out_temp[1];
+      issuemult_out_temp_r[0] <= (reset) ? 'h0 : issuemult_out_temp[0];
+
+      issuemult_tag_temp_r[2] <= (reset) ? 'h0 : issuemult_tag_temp[2];
+      issuemult_tag_temp_r[1] <= (reset) ? 'h0 : issuemult_tag_temp[1];
+      issuemult_tag_temp_r[0] <= (reset) ? 'h0 : issuemult_tag_temp[0];
    end
 
-   always @(posedge clk) begin : mult_done
-      issuemult_equeuemult_done_r <= (reset) ? 0 : issuemult_equeuemult_done;
+   always @(posedge clk) begin : issue_int_before_ls_reg_assign
+      int_before_ls_r <= (reset) ? 1'b0 : int_before_ls;
    end
 
-   always @(*) begin: mult_done_proc
-      issuemult_equeuemult_done = issuemult_equeuemult_done_r;
+   always @(posedge clk) begin : issue_cdb_data_reg_assign
+      cdb_data_oreg_r         <= (reset) ? 'h0 : cdb_data_oreg;
+      cdb_valid_oreg_r        <= (reset) ? 'h0 : cdb_valid_oreg;
+      cdb_data_oreg_r         <= (reset) ? 'h0 : cdb_data_oreg;
+      cdb_tag_oreg_r          <= (reset) ? 'h0 : cdb_tag_oreg;
+      cdb_branch_oreg_r       <= (reset) ? 'h0 : cdb_branch_oreg;
+      cdb_branch_taken_oreg_r <= (reset) ? 'h0 : cdb_branch_taken_oreg;
    end
 
-   // Module instantiations
-   // integer exec unit
+   always @(posedge clk) begin : cdb_slots_reg_assign
+      cdb_slot_r [6] <= (reset) ? 'h0 :  cdb_slot [6] ;
+      cdb_slot_r [5] <= (reset) ? 'h0 :  cdb_slot [5] ;
+      cdb_slot_r [4] <= (reset) ? 'h0 :  cdb_slot [4] ;
+      cdb_slot_r [3] <= (reset) ? 'h0 :  cdb_slot [3] ;
+      cdb_slot_r [2] <= (reset) ? 'h0 :  cdb_slot [2] ;
+      cdb_slot_r [1] <= (reset) ? 'h0 :  cdb_slot [1];
+      cdb_slot_r [0] <= (reset) ? 'h0 :  cdb_slot [0];
+   end
+
+   always @(posedge clk) begin : issue_div_reg_assign
+      div_cdb_ctrl_r[5] <= (reset) ? 'h0 : div_cdb_ctrl[5];
+      div_cdb_ctrl_r[4] <= (reset) ? 'h0 : div_cdb_ctrl[4];
+      div_cdb_ctrl_r[3] <= (reset) ? 'h0 : div_cdb_ctrl[3];
+      div_cdb_ctrl_r[2] <= (reset) ? 'h0 : div_cdb_ctrl[2];
+      div_cdb_ctrl_r[1] <= (reset) ? 'h0 : div_cdb_ctrl[1];
+      div_cdb_ctrl_r[0] <= (reset) ? 'h0 : div_cdb_ctrl[0];
+   end
+
+   always @(posedge clk) begin : issue_mult_reg_assign
+      mult_cdb_ctrl_r[2] <= (reset) ? 'h0 : mult_cdb_ctrl[2];
+      mult_cdb_ctrl_r[1] <= (reset) ? 'h0 : mult_cdb_ctrl[1];
+      mult_cdb_ctrl_r[0] <= (reset) ? 'h0 : mult_cdb_ctrl[0];
+   end
+
    issueint issueint (
       .clk                       (clk                     ),
       .reset                     (reset                   ),
@@ -240,34 +252,22 @@ module issue (
       .issueint_alubranch        (issueint_alubranch      ),
       .issueint_alubranch_taken  (issueint_alubranch_taken)
    );
-   // divider exec unit assign issuediv_out = div_res;
-   /*divider issuediv (
-      .clk_div          (clk),
-      .reset_div        (~reset),
-      .issueque_div_a   (issuediv_rsdata),
-      .issueque_div_b   (issuediv_rtdata),
-      .issueque_div_tag (issuediv_rdtag),
-      .issue_div        (1'b1),
-      .div_tag_out      (issuediv_tagout),
-      .div_res          (issuediv_out),
-      .div_rfd          (div_exec_ready)
-   ); */
-   divider_wrapper divider_wrapper(
-         .clk                 (clk              ),
-         .reset               (reset            ),
-         .issuediv_enable     (issue_div),
-         .issuediv_rsdata     (issuediv_rsdata  ),
-         .issuediv_rtdata     (issuediv_rtdata  ),
-         .issuediv_rdtag      (issuediv_rdtag   ),
 
-         .issuediv_busy       (div_exec_ready   ),
-         .issuediv_out        (issuediv_out),
-         .issuediv_rdtag_out  (issuediv_tagout  )
+   divider_wrapper divider_wrapper(
+      .clk                 (clk              ),
+      .reset               (reset            ),
+      .issuediv_enable     (issue_div),
+      .issuediv_rsdata     (issuediv_rsdata  ),
+      .issuediv_rtdata     (issuediv_rtdata  ),
+      .issuediv_rdtag      (issuediv_rdtag   ),
+
+      .issuediv_busy       (div_exec_ready   ),
+      .issuediv_out        (issuediv_out),
+      .issuediv_rdtag_out  (issuediv_tagout  )
    );
-   //multiplier exec unit
+
    multiplier_wrapper multiplier_wrapper(
       .clk                 (clk              ),
-     // .reset             (reset            ),
       .issuemult_rsdata    (issuemult_rsdata ),
       .issuemult_rtdata    (issuemult_rtdata ),
       .issuemult_rdtag     (issuemult_rdtag  ),
@@ -275,10 +275,13 @@ module issue (
       .issuemult_out       (issuemult_out    ),
       .issuemult_rdtag_out (issuemult_tagout )
    );
-   //load/store exec unit
+
+   //
+   // Opcode is translated as write enable.
+   //
    dcache dcache (
       .clk           (clk             ),
-      .wen           (issuels_opcode  ), //opcode
+      .wen           (issuels_opcode  ),
 
       .addr          (issuels_addr    ),
       .wdata         (issuels_data    ),
